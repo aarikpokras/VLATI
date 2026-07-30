41fca5044e7d144daaffcd51aa6e05ae8cd1a705
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2026-07-30 00:05:18 -0400

    Config file
    All params here are required (as of now; might change this later)

---
+++ conf.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+### CONFIG ###
+
+conf = {
+  "frames_to_simulate": 500000,
+  "seconds_timestep": 1,
+  "degrees_moon_start_angle_N": 0,
+  "meters_spacecraft_start_vec": np.array([6537000.0, 0.0]),
+  "meters_per_second_spacecraft_start_v_vec": np.array([0.0, 11000.0])
+}
+
+##############
