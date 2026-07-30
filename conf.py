ed37d24b0cc0ddda7171875ecd6a7476d4078c7b
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2026-07-30 19:14:59 -0400

    Add settings for burns

--- conf.py
+++ conf.py
@@ -7,7 +7,10 @@ conf = {
   "seconds_timestep": 1,
   "degrees_moon_start_angle_N": 0,
   "meters_spacecraft_start_vec": np.array([6537000.0, 0.0]),
-  "meters_per_second_spacecraft_start_v_vec": np.array([0.0, 11000.0])
+  "meters_per_second_spacecraft_start_v_vec": np.array([0.0, 11200.0]),
+  "bool_burn": True,
+  "meters_per_second_delta_v_burn": np.array([1000.0, 0.0]),
+  "seconds_burn_time_elapsed": 200000,
 }
 
 ##############
