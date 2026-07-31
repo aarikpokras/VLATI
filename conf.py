8e6d63dea6ab4d463927a785b3dd26a09e758511
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2026-07-30 21:49:17 -0400

    dv burn boolean to false
    Don't want to assume anything

--- conf.py
+++ conf.py
@@ -8,7 +8,7 @@ conf = {
   "degrees_moon_start_angle_N": 0,
   "meters_spacecraft_start_vec": np.array([6537000.0, 0.0]),
   "meters_per_second_spacecraft_start_v_vec": np.array([0.0, 11200.0]),
-  "bool_burn": True,
+  "bool_burn": False,
   "meters_per_second_delta_v_burn": np.array([1000.0, 0.0]),
   "seconds_burn_time_elapsed": 200000,
 }
