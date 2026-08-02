f3cb55ba527f9e7b291e8be017e50f861e79c7ee
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2026-08-01 23:14:10 -0400

    Add trajectory output to conf file

--- conf.py
+++ conf.py
@@ -3,9 +3,11 @@ import numpy as np
 ### CONFIG ###
 
 conf = {
+  "traj_output_file": "./output.npz",
   "frames_to_simulate": 500000,
   "seconds_timestep": 1,
   "degrees_moon_start_angle_N": 0,
+  "degrees_sun_start_angle_N": 0,
   "meters_spacecraft_start_vec": np.array([6537000.0, 0.0]),
   "meters_per_second_spacecraft_start_v_vec": np.array([0.0, 11200.0]),
   "bool_burn": False,
