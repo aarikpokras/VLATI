a237ecf86efa5253c10c5759897cfa65b695703c
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2026-08-03 11:48:50 -0400

    Change sc vector np.array decs to normal lists (conversions done in main)

--- conf.py
+++ conf.py
@@ -8,8 +8,8 @@ conf = {
   "seconds_timestep": 1,
   "degrees_moon_start_angle_N": 0,
   "degrees_sun_start_angle_N": 0,
-  "meters_spacecraft_start_vec": np.array([6537000.0, 0.0]),
-  "meters_per_second_spacecraft_start_v_vec": np.array([0.0, 11200.0]),
+  "meters_spacecraft_start_vec": [6537000.0, 0.0],
+  "meters_per_second_spacecraft_start_v_vec": [0.0, 11200.0],
   "burn_array": ( [200000, np.array([1000.0, 0.0]), 0], [300000, np.array([1500.0, 0.0]), 0] )
 }
 
