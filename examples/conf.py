3ee727985181aff6442add93658b4462f92c0c03
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2026-08-03 15:45:44 -0400

    Update conf.py

--- examples/conf.py
+++ examples/conf.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-# A configuration file for a simple, distant lunar flyby.
+# A configuration file for a simple lunar flyby with a
+# perilune of about 5000km.
 # To calculate this, I used Kepler's equations and the vis
 # viva equation. It is the config file used for the gif on
 # the README.
@@ -13,7 +14,7 @@ conf = {
   "degrees_moon_start_angle_N": 24.75684,
   "degrees_sun_start_angle_N": 0,
   "meters_spacecraft_start_vec": [-6537000.0, 0.0],
-  "meters_per_second_spacecraft_start_v_vec": [0.0, -10983.0],
+  "meters_per_second_spacecraft_start_v_vec": [0.0, -10977.0],
   #"burn_array": ( [200000, np.array([1000.0, 0.0]), 0], [300000, np.array([1500.0, 0.0]), 0] )
   "burn_array": ()
 }
