8c38311171cb527746a640857c6a591267c2acb5
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2026-08-04 00:24:26 -0400

    3D trajectories in VLATI-VIS

--- vlati-vis.py
+++ vlati-vis.py
@@ -24,38 +24,47 @@ traj_sc = trajs["sc"]
 traj_moon = trajs["moon"]
 traj_sun = trajs["sun"]
 
-fig, ax = plt.subplots()
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
 
 ax.set_xlim(-120000000, 120000000)
 ax.set_ylim(-120000000, 120000000)
+ax.set_zlim(-120000000, 120000000)
 ax.set_aspect('equal')
 
-traj_sc_line = plt.plot(traj_sc[:,0], traj_sc[:,1], 'm-')
-traj_moon_line = plt.plot(traj_moon[:,0], traj_moon[:,1], 'g-')
-traj_sun_line = plt.plot(traj_sun[:,0], traj_sun[:,1], 'k-')
+traj_sc_line = ax.plot(traj_sc[:,0], traj_sc[:,1], traj_sc[:,2], 'm-')
+traj_moon_line = ax.plot(traj_moon[:,0], traj_moon[:,1], traj_moon[:,2], 'g-')
+traj_sun_line = ax.plot(traj_sun[:,0], traj_sun[:,1], traj_sun[:,2], 'k-')
 
-sc_dot, = ax.plot([traj_sc[0][0]], [traj_sc[0][1]], 'g^')
-moon_dot, = ax.plot([traj_moon[0][0]], [traj_moon[0][1]], 'ko')
-sun_dot, = ax.plot([traj_sun[0][0]], [traj_sun[0][1]], 'yo')
-earth_dot = ax.plot([0], [0], 'bo')
+sc_dot, = ax.plot([traj_sc[0][0]], [traj_sc[0][1]], [traj_sc[0][2]], 'g^')
+moon_dot, = ax.plot([traj_moon[0][0]], [traj_moon[0][1]], [traj_moon[0][2]], 'ko')
+sun_dot, = ax.plot([traj_sun[0][0]], [traj_sun[0][1]], [traj_sun[0][2]], 'yo')
+earth_dot = ax.plot([0], [0], [0], 'bo')
 
 moonfmts = "MOON     "
 earthfmts = "EARTH    "
 sunfmts = "SUN/1000 "
 
-moon_ro = ax.text(0.02, 0.02, moonfmts, fontsize=10, transform=ax.transAxes, fontfamily='DejaVu Sans Mono')
-earth_ro = ax.text(0.02, 0.08, earthfmts, fontsize=10, transform=ax.transAxes, fontfamily='DejaVu Sans Mono')
-sun_ro = ax.text(0.02, 0.14, sunfmts, fontsize=10, transform=ax.transAxes, fontfamily='DejaVu Sans Mono')
+moon_ro = ax.text2D(0.02, 0.02, moonfmts, fontsize=10, transform=ax.transAxes, fontfamily='DejaVu Sans Mono')
+earth_ro = ax.text2D(0.02, 0.08, earthfmts, fontsize=10, transform=ax.transAxes, fontfamily='DejaVu Sans Mono')
+sun_ro = ax.text2D(0.02, 0.14, sunfmts, fontsize=10, transform=ax.transAxes, fontfamily='DejaVu Sans Mono')
 
 def update(frame):
   t_ = frame * MULTIPLIER
   moon_ro.set_text(f"{moonfmts}{np.linalg.norm(traj_sc[t_] - traj_moon[t_]):.11f}")
-  earth_ro.set_text(f"{earthfmts}{np.linalg.norm(traj_sc[t_] - np.array([0, 0])):.11f}")
+  earth_ro.set_text(f"{earthfmts}{np.linalg.norm(traj_sc[t_] - np.array([0, 0, 0])):.11f}")
   sun_ro.set_text(f"{sunfmts}{np.linalg.norm(traj_sc[t_] - traj_sun[t_])/1000:.11f}")
   plt.xlabel(f"t = {t_}", fontsize=10, fontfamily='DejaVu Sans Mono', loc='left')
+
   sc_dot.set_data([traj_sc[t_][0]], [traj_sc[t_][1]])
+  sc_dot.set_3d_properties([traj_sc[t_][2]])
+
   moon_dot.set_data([traj_moon[t_][0]], [traj_moon[t_][1]])
+  moon_dot.set_3d_properties([traj_moon[t_][2]])
+
   sun_dot.set_data([traj_sun[t_][0]], [traj_sun[t_][1]])
+  sun_dot.set_3d_properties([traj_sun[t_][2]])
+
   return sc_dot, moon_dot, sun_dot
 
 anim = FuncAnimation(fig, update, frames=range(start_frame, end_frame), interval=0, blit=False, repeat=True)
