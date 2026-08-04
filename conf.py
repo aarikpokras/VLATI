7e8e668e084698ce262ef639f1a8d0dc70493476
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2026-08-04 00:25:34 -0400

    3D trajectories updates in conf.py

--- conf.py
+++ conf.py
@@ -6,11 +6,11 @@ conf = {
   "traj_output_file": "./output.npz",
   "frames_to_simulate": 500000,
   "seconds_timestep": 1,
-  "degrees_moon_start_angle_N": 0,
+  "degrees_moon_start_angle_N": 24.75684,
   "degrees_sun_start_angle_N": 0,
-  "meters_spacecraft_start_vec": [6537000.0, 0.0],
-  "meters_per_second_spacecraft_start_v_vec": [0.0, 11200.0],
-  "burn_array": ( [200000, np.array([1000.0, 0.0]), 0], [300000, np.array([1500.0, 0.0]), 0] )
+  "meters_spacecraft_start_vec": [-6537000.0, 0.0, 0.0],
+  "meters_per_second_spacecraft_start_v_vec": [0.0, 3000.0, 100.0],
+  "burn_array": ( [273000, np.array([100.0, -2000.0, 0.0]), 0], [0, 0, 1] )
 }
 
 ##############
